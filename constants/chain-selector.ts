e8714c9aeabaf05e8c84f27b907ee3cbb2750e94
ribbinpo <ribbinpo@gmail.com>
2023-11-02 22:26:50 +0700

    chore: :wrench: add router and token of ccip address

---
+++ constants/chain-selector.ts
@@ -0,0 +1,9 @@
+export const CHAIN_SELECTOR = {
+  mumbai: "12532609583862916517",
+  sepolia: "16015286601757825753",
+  fuji: "14767482510784806043",
+  "optimism-goerli": "2664363617261496610",
+  "arbitrum-goerli": "6101244977088475029",
+  "bsc-testnet": "13264668187771770619",
+  "base-goerli": "13264668187771770619",
+};
